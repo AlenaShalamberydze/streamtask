d7f8e5643beb0ea4490fd6e37c5dfdd3eb611704
Alena Shalamberydze <Alena.Shalamberydze@LeverX.local>
2021-02-15 12:28:55 +0300

    grouping by parameters for tasks added

--- streamtask/src/main/java/leverx/example/stream/task2/TaskSearchApp.java
+++ streamtask/src/main/java/leverx/example/stream/task2/TaskSearchApp.java
@@ -1,12 +1,19 @@
 package leverx.example.stream.task2;
 
-import static leverx.example.stream.task2.service.TaskService.filterTaskListByDate;
+import leverx.example.stream.task2.entity.Task;
+
+import java.util.List;
+
+import static leverx.example.stream.task2.service.TaskService.*;
 import static leverx.example.stream.task2.service.TaskServiceBuilder.createTaskList;
 
 public class TaskSearchApp {
     public static void main(String[] args) {
+        List<Task> tasks = createTaskList();
 
-        System.out.println(filterTaskListByDate(createTaskList(), 6));
+        System.out.println(filterTaskListByDate(tasks, 6));
+
+        System.out.println(groupTasksByParameters(tasks));
     }
 
 }
--- streamtask/src/main/java/leverx/example/stream/task2/service/TaskService.java
+++ streamtask/src/main/java/leverx/example/stream/task2/service/TaskService.java
@@ -1,9 +1,13 @@
 package leverx.example.stream.task2.service;
 
 import leverx.example.stream.task2.entity.Task;
+import leverx.example.stream.task2.entity.TaskType;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class TaskService {
@@ -15,4 +19,15 @@ public class TaskService {
                 .map(Task::getTitle)
                 .collect(Collectors.joining(", "));
     }
+
+    public static Map<String,
+            Map<TaskType, Map<LocalDate, Map<Set<String>, List<Task>>>>> groupTasksByParameters(List<Task> tasks) {
+        return tasks.stream()
+                .collect(Collectors.groupingBy(Task::getTitle,
+                        Collectors.groupingBy(Task::getType,
+                                Collectors.groupingBy(Task::getCreatedOn,
+                                        Collectors.groupingBy(Task::getTags)))));
+
+    }
+
 }
